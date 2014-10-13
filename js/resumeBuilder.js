4e842fb1d93ef6e1a0c7c0b456078e4fb6b566ef
Glenn Cameron <invest@eggify.com>
2014-10-12 17:09:01 -0700

    Completed tutorials
    Ready for customization

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,22 @@
-var formattedName = HTMLheaderName.replace("%data%", "Glenn Cameron");
+var formattedName = HTMLheaderName.replace("%data%", inName("glenn cameron"));
 var formattedRole = HTMLheaderRole.replace("%data%", "Tinkerer");
-var bioPic = HTMLbioPic.replace("%data%", "images/fry.jpg");
+var bioPic = HTMLbioPic.replace("%data%", "images/glenn.jpg");
+var formattedMobile = HTMLmobile.replace("%data%", "424.234.8852");
+var formattedEmail = HTMLemail.replace("%data%", "glenn@glenncameronjr.com");
+var formattedTwitter = HTMLtwitter.replace("%data%", "@glenncameronjr");
+var formattedLocation = HTMLlocation.replace("%data%", "Sunnyvale, CA");
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%", "Welcome to my resumish website");
 
-$(header).append(formattedName);
-$(header).append(formattedRole);
-$(header).append(bioPic);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+$("#header").append(bioPic);
+$("#header").append(formattedWelcome);
+
+
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedLocation);
 
 
 var  bio = {
@@ -24,6 +36,7 @@ var  bio = {
             "Inventions"
         ]
     }
+
 
 //JSON format
 
@@ -87,16 +100,20 @@ var projects =
             "title": "Samsung Developer Conference i",
             "location": "San Francisco, California",
             "dates": "2013",
-            "desc": "Samsungs first international developer conference"
+            "desc": "Samsungs first international developer conference",
+            "image": "http://lorempixel.com/400/200/"
         },
         {
             "title": "Samsung Developer Conference ii",
             "location": "San Francisco, California",
             "dates": "2013",
-            "desc": "Samsungs second international developer conference"
+            "desc": "Samsungs second international developer conference",
+            "image": "http://lorempixel.com/400/201/"
         }
     ]
 }
+
+var locations = ["38.026332, -122.424909"];
 
 /*
 $("#header").append(HTMLskillsStart);
@@ -122,18 +139,66 @@ if (bio.skills.length > 0) {
 	$("#skills").append(formattedSkill);
 
 }
+function displayWork() {
+	for(job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
 
-for(job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedDesc = HTMLprojectDescription.replace("%data%", work.jobs[job].desc_1);
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	var formattedDesc = HTMLprojectDescription.replace("%data%", work.jobs[job].desc_1);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedDates + formattedDesc;
 
-	var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedDates + formattedDesc;
-
-	$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedEmployerTitle);
+	}
 }
 
+displayWork();
+
+function displayProjects() {
+    for (project in projects.projects) {
+         $("#projects").append(HTMLprojectStart);
+        
+
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        //var formattedProjectLocation = HTMLprojectLocationreplace("%data%", projects.projects[project].location);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].desc);
+        var formattedProjectImage= HTMLprojectImage.replace("%data%", projects.projects[project].image);
+        
+
+        var formattedProjectFull = formattedProjectTitle + formattedProjectDates + formattedProjectDescription + formattedProjectImage;
+
+       $(".project-entry:last").append(formattedProjectFull);
+    }
+}
+
+displayProjects();
+
+
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x,y);
+    });
+
+$("#main").append(internationalizeButton);
+
+function inName(name) {
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0] + " " + name[1];
+}
+
+$("#main").append(internationalizeButton);
+
+inName("glenn cameron");
+
+$("#mapDiv").append(googleMap);
 
